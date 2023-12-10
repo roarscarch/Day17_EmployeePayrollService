4f12ad7ff3b828f3ce56b5b571dfc9f11b3a826b
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-10 18:26:47 +0530

    added updation

--- src/main/java/org/example/DataBaseConnectivity.java
+++ src/main/java/org/example/DataBaseConnectivity.java
@@ -1,14 +1,17 @@
 package org.example;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Scanner;
 
 public class DataBaseConnectivity {
+
     public static void main(String[] args) throws SQLException {
         JDBC jdbc = new JDBC();
         Connection connection = jdbc.getConnection();
@@ -17,6 +20,9 @@ public class DataBaseConnectivity {
         // Example: Call the readEmployee method using the established connection
         DataBaseConnectivity dbConnectivity = new DataBaseConnectivity();
         dbConnectivity.readEmployee(connection);
+
+        // Example: Call the updateData method using the established connection
+        updateData(connection);
     }
 
     public void readEmployee(Connection connection) {
@@ -31,13 +37,54 @@ public class DataBaseConnectivity {
                 double salary = resultSet.getDouble("salary");
                 Date startDate = resultSet.getDate("startDate");
 
-                Employee employee = new Employee(id, name,salary, startDate);
+                Employee employee = new Employee(id, name, salary, startDate);
                 employeeList.add(employee);
 
                 System.out.println("ID: " + id + ", Name: " + name + ", Salary: " + salary + ", StartDate: " + startDate);
             }
         } catch (SQLException exception) {
             exception.printStackTrace();
+        }
+    }
+
+    public static void updateData(Connection connection) {
+        Scanner sc = new Scanner(System.in);
+        PreparedStatement preparedStatement = null;
+
+        try {
+            preparedStatement = connection.prepareStatement("UPDATE employee_payroll SET salary = ? WHERE id = ?;");
+
+            System.out.println("Enter salary to be updated: ");
+            double salary = sc.nextDouble();
+            System.out.println("Enter at which id you want to update salary: ");
+            int id = sc.nextInt();
+
+            preparedStatement.setDouble(1, salary);
+            preparedStatement.setInt(2, id);
+
+            int rowsAffected = preparedStatement.executeUpdate();
+
+            if (rowsAffected > 0) {
+
+
+                System.out.println("Updated Successfully!!!");
+
+            } else {
+                System.out.println("No employee found with the provided ID.");
+            }
+        } catch (SQLException | java.util.InputMismatchException exception) {
+            exception.printStackTrace();
+        } finally {
+            // Close resources (e.g., PreparedStatement and Scanner)
+            try {
+                if (preparedStatement != null) {
+                    preparedStatement.close();
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
+            sc.close();
         }
     }
 }
