0cb04bf0e528e5447ea08e45d0d1fc4eef8883bc
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-10 14:00:23 +0530

    database connection

--- src/main/java/org/example/DataBaseConnectivity.java
+++ src/main/java/org/example/DataBaseConnectivity.java
@@ -6,7 +6,8 @@ import java.sql.SQLException;
 public class DataBaseConnectivity {
     public static void main(String[] args) throws SQLException {
 
-        System.out.println("Learning JDBC");
-
+      JDBC jdbc = new JDBC();
+      Connection connection = jdbc.getConnection();
+      System.out.println("Database connection to payroll_service established.");
     }
 }
---
+++ src/main/java/org/example/JDBC.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class JDBC {
+    public Connection getConnection()throws SQLException {
+        String url = "jdbc:mysql://localhost:3306/payroll_service";
+        String user = "root";
+        String password = "aNUR@9262";
+        return DriverManager.getConnection(url,user,password);
+    }
+
+}
