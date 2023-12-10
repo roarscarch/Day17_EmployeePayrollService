e23b2f00f4751310a4242d9eaef1c96e6359cd78
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-10 13:55:14 +0530

    learning JDBC

---
+++ src/main/java/org/example/DataBaseConnectivity.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataBaseConnectivity {
+    public static void main(String[] args) throws SQLException {
+
+        System.out.println("Learning JDBC");
+
+    }
+}
