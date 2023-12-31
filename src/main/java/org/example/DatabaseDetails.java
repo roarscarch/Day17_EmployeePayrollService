be8a440b41cfd22741824c82e26b180abc3e3bbf
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-31 20:10:16 +0530

    update

--- src/main/java/org/example/DatabaseDetails.java
+++ src/main/java/org/example/DatabaseDetails.java
@@ -3,5 +3,5 @@ package org.example;
 public class DatabaseDetails {
     public static String URL = "jdbc:mysql://localhost:3306/payroll_service";
     public static String USER = "root";
-    public static String PASSWORD = "aNUR@9262";
+    public static String PASSWORD = "password";
 }
