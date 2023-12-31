d742da3f7d35f8076ff0edd72294da3977fbad7a
roarscarch <2020ugee082@nitjsr.ac.in>
2023-12-31 20:07:52 +0530

    promypass

--- src/main/java/org/example/DatabaseDetails.java
+++ src/main/java/org/example/DatabaseDetails.java
@@ -4,5 +4,5 @@ package org.example;
 public class DatabaseDetails {
     static final String URL="jdbc:mysql://localhost:3306/payroll_service";
     static final String USER="root";
-    static final String PASSWORD="aNUR@9262";
+    static final String PASSWORD="password";
 }
